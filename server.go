25a646ea0d0d13357858103459aa721e85be7414
1stepanviolet1 <stepanviolet@gmail.com>
2023-10-11 11:15:37 +0300

    delete unnecessary code

--- server.go
+++ server.go
@@ -3,51 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 )
 
-func send_index(w http.ResponseWriter, r *http.Request) {
-	html_index := get_index()
-	w.Write(html_index)
-
-}
-
-func send_styles(w http.ResponseWriter, r *http.Request) {
-	styles := get_styles()
-	w.Write(styles)
-
-}
-
 func main() {
-	// http.HandleFunc("/", send_index)
-
 	http.Handle("/", http.FileServer(http.Dir("html/")))
 
 	log.Println("Start server from localhost:8080")
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
-
-}
-
-func get_index() []byte {
-	html, err := os.ReadFile("./html/index.html")
-
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-
-	return html
-
-}
-
-func get_styles() []byte {
-	styles, err := os.ReadFile("./html/style.css")
-
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-
-	return styles
 
 }
